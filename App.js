e49fd484f817ad4404aa155cf625dc24f8a8fdfb
Wong, Tien-Yu <149470955+Jasmine091203@users.noreply.github.com>
2024-06-18 15:50:55 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -30,9 +30,6 @@ function App() {
     return (
         <div className="App">
             <RouterProvider router={router} />
-            {/* react-router-dom 套件 裡面的 元件'RouterProvider'
-            並沒有實際寫到它的程式碼 /}
-            {/ 指引到對應的頁面的元件是RouterProvider，它的屬性是router={} */}
         </div>
     );
 }
